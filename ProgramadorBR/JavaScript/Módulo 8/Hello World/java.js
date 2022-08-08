3b12d3f85434a67c8265c81648f79d3fa2c04a27
Lucas Brasileiro <97102750+llucasbrasil@users.noreply.github.com>
2022-08-08 14:31:53 -0300

    atualizando

--- ProgramadorBR/JavaScript/Módulo 8/Hello World/java.js
+++ ProgramadorBR/JavaScript/Módulo 8/Hello World/java.js
@@ -1,8 +1,16 @@
-var a =96;
-a %= 30;
+// var a =96;
+// a %= 30;
 
 
-var b = 96 % 30;
+// var b = 96 % 30;
+
+// var str = "Valor 'qualquer'" ;
+// str += " ";
+// str += "outro texto";
 
 
-console.log(a);
+// console.log(str);
+
+var num = prompt("Digite um número");
+var double = num * 2;
+alert("O dobro de "+num+" é "+ double);
