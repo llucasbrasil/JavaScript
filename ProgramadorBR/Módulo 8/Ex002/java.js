374615fbcee019bfb238413371fb5550e2f5d3ac
Lucas Brasileiro <97102750+llucasbrasil@users.noreply.github.com>
2022-08-08 23:32:58 -0300

    a

---
+++ ProgramadorBR/Módulo 8/Ex002/java.js
@@ -0,0 +1,27 @@
+
+// var idade = 50;
+
+// if (idade >= 35){
+//     console.log ("Pode");
+//     console.log ("Qual o seu pedido?");
+// }
+// else if (idade >= 18){
+//     console.log ("Pode");
+//     console.log ("Mostre a ID!");
+// }
+
+// else {
+//     console.log ("Não pode");
+//     console.log ("Volte futuramente");
+// }
+
+var idade = 20;
+
+if (idade > 18 && idade < 70){
+    console.log('Pode');
+    console.log('Qual o seu pedido?');
+}
+else {
+    console.log('Não pode!');
+    console.log('Volte futuramente');
+}
