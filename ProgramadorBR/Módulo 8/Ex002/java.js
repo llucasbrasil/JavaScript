94b98fda4395510b404571666c26ecc8b95373e7
Lucas Brasileiro <97102750+llucasbrasil@users.noreply.github.com>
2022-08-10 11:43:12 -0300

    a

--- ProgramadorBR/Módulo 8/Ex002/java.js
+++ ProgramadorBR/Módulo 8/Ex002/java.js
@@ -15,13 +15,46 @@
 //     console.log ("Volte futuramente");
 // }
 
-var idade = 20;
+// var idade = 20;
 
-if (idade > 18 && idade < 70){
-    console.log('Pode');
-    console.log('Qual o seu pedido?');
-}
-else {
-    console.log('Não pode!');
-    console.log('Volte futuramente');
-}
+// if (idade > 18 && idade < 70){
+//     console.log('Pode');
+//     console.log('Qual o seu pedido?');
+// }
+// else {
+//     console.log('Não pode!');
+//     console.log('Volte futuramente');
+// }
+
+
+
+// var age = 30;
+
+// if (age >= 35){
+//     console.log('Pode')
+//     console.log('Qual o seu pedido?')
+// }
+// else if(age >= 18){
+//     console.log('Pode')
+//     console.log('Preciso de sua ID')
+// }
+// else{
+//     console.log('Não pode!')
+//     console.log('Volte daqui alguns anos')
+// }
+
+
+// var age = 16;
+// if (age >= 17 && age <= 70){
+//     console.log('Permitido comprar')}
+// else{
+//     console.log('Não pode filho')
+//     console.log('Apenas com o seu responsável')}
+
+
+var age = 71;
+if (age >= 18 && age <= 70){
+    console.log('Permitido o acesso')}
+else{
+    console.log('Acesso negado')
+    console.log('Volte acompanhado de um responsável')}
---
+++ ProgramadorBR/Módulo 8/Ex003/java.js
@@ -0,0 +1,58 @@
+// var nota1 = 7;
+// var nota2 = 6.5;
+// var nota3 = 5.5;
+// var nota4 = 9;
+
+// var media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+// if ( media >= 7){
+//     console.log("Você foi aprovado!");
+//     console.log("Obteve a média de " + media);
+// }
+// else{
+//     console.log("Você não foi aprovado");
+//     console.log("Sua média foi " + media + " ,inferior a '7'");
+// }
+
+var n1 = 5;
+var n2 = 8;
+var n3 = 7;
+var n4 = 9;
+
+var media = (n1 + n2 + n3 + n4) / 4;
+var conceito = ""
+
+if (media >= 9){
+    conceito = "Excelente";
+}
+else if (media >= 8){
+    conceito = "Ótimo";
+}
+else if (media >= 7){
+    conceito = "Bom";
+}
+else{
+    conceito = "Ruim";
+}
+
+console.log("Conceito: " + conceito);
+console.log ("Média obtida nos 4 semestres de: " + parseInt(media));
+
+switch (conceito){
+    case "Excelente":
+        console.log("Parabéns, você é pica de ouro!");
+        break;
+    case "Ótimo":
+        console.log("Você está ficando dahora bruxão");
+        break;
+    case "Bom":
+        console.log("É... Você passou com a água batendo na bunda, mas foi...");
+        break;
+    case "Ruim":
+        console.log("Ah burro! Vai estudar mais filho, se lascou bonito");
+        break;
+    default:
+        console.log("Houve algum erro!");
+        break;
+}
+    
