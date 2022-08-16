454708b280ef16b968c771e7fb50481ca8a5a60c
Lucas Brasileiro <97102750+llucasbrasil@users.noreply.github.com>
2022-08-15 23:43:38 -0300

    atualizado

---
+++ ProgramadorBR/Módulo 8/Ex004/java.js
@@ -0,0 +1,47 @@
+// var num = 2;
+
+// for (var i = 0; ; i ++){
+
+//     console.log("Executando " + (i));
+    
+// }
+//     console.log("Fim");
+
+
+// for (var i = 5; i < 9 ; i++){
+//     console.log(i);
+
+// }
+
+// console.log("Fim")
+
+
+
+
+// var i = 0;
+// for(;i<9;i++){
+//     console.log(i);
+
+// }
+
+for (var i =0;;i++){
+    console.log(i);
+    if (i>9)break;
+}
+
+
+
+
+
+
+
+
+// var num = Math.random() * 100;
+
+// while ( num < 90 ){
+//     console.log("Número " + num );
+//     num = Math.random ()* 100;
+
+// }
+
+//     console.log("Fim");
